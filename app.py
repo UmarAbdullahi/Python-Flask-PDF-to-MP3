f2120dbcd77d1d310ed36e7230e6699b59a0f175
umarabdullahi <u.abdullahi001@gmail.com>
2020-11-08 23:37:50 +0100

    changes

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 # Importing the necessary Libraries
+import os
+import os.path
 from flask_cors import cross_origin
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from main import text_to_speech
 
-# import request
 app = Flask(__name__)
+#os.path.abspath()
 
 @app.route('/', methods=['POST', 'GET'])
 @cross_origin()
 def homepage():
     if request.method == 'POST':
-        text = request.form['speech']
-        gender = request.form['voices']
-        text_to_speech(text, gender)
-        return render_template('frontend.html')
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            uploaded_file.save(uploaded_file.filename)
+            cwd = os.path.abspath(uploaded_file.filename)
+            
+            text = cwd
+            gender = request.form['voices']
+            text_to_speech(text, gender)
+        return redirect(url_for('homepage'))
     else:
         return render_template('frontend.html')
 
